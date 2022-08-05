5352b8e024a674fa09ea9bdfade0e572c9bb967f
zhangrenLiu <zhangrenliu@zhangrenLius-MacBook-Pro.local>
2022-08-05 17:41:32 +0800

    [zhangren] remove font

--- dist/dom2image.js
+++ dist/dom2image.js
@@ -59,7 +59,6 @@
             .then(function (node) {
                 return cloneNode(node, options.filter, true);
             })
-            .then(embedFonts)
             .then(inlineImages)
             .then(applyOptions)
             .then(function (clone) {
